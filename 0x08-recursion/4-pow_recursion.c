0928707986f2c99f0781acbe154f87280bef9087
Mohamed <mr.elasyes.projects@gmai.com>
2023-10-25 20:22:59 +0300

    fixed "control reaches end of non-void function"

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -7,14 +7,12 @@
 */
 int _pow_recursion(int x, int y)
 {
-	int i = x;
-
 	if (y == 1)
-		return (i);
+		return (x);
 	else if (y == 0)
 		return (1);
 	else if (y < 0)
 		return (-1);
-	else if (y > 1)
+	else
 		return (x * _pow_recursion((x), (y - 1)));
 }
