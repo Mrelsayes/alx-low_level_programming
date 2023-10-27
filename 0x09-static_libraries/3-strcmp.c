3871422773812708a14a89215c246cc430dcf971
Mohamed <mr.elasyes.projects@gmai.com>
2023-10-27 15:00:30 +0200

    hot fix

--- 0x09-static_libraries/3-strcmp.c
+++ 0x09-static_libraries/3-strcmp.c
@@ -15,3 +15,23 @@
 
 	   • a positive value if s1 is greater than s2.
 */
+int _strcmp(char *s1, char *s2)
+{
+	int len1 = 0;
+	int len2 = 0;
+
+	for (; s1[len1] <= '\0'; len1++);
+	for (; s2[len2] <= '\0'; len2++);
+	if (s1[len1] == s2[len2])
+	{
+		return (0);
+	}
+	else if (s1[len1] - s2[len2] < 0)
+	{
+		return (s1[len1] - s2[len2]);
+	}
+	else if (s1[len1] - s2[len2] > 0)
+	{
+		return (s1[len1] - s2[len2]);
+	}
+}
