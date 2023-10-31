94a6922fe23e9dc81baee2989a802459d7d9c757
Mohamed <138699331+Mrelsayes@users.noreply.github.com>
2023-10-31 23:46:10 +0300

    fix task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,15 +10,14 @@ char *str_concat(char *s1, char *s2)
 	char *a;
 	int str_len, count, count2;
 
-	for (str_len = 0; s1[str_len] || s2[str_len]; str_len++)
-	;
-	a = malloc(str_len * sizeof(char));
-	if (s1 == 0)
+	if (s1 == NULL)
 		s1 = "";
-	if (s2 == 0)
+	if (s2 == NULL)
 		s2 = "";
+	for (str_len = 0; s1[str_len] || s2[str_len]; str_len++)
+	a = malloc(str_len * sizeof(char));
 	if (a == NULL)
-	return (NULL);
+		return (NULL);
 	for (count = 0; s1[count]; count++)
 	{
 		if (s1[count] == 0)
