464ec713361b4d8098c8e00e20f522cf88229ed2
Mohamed <mr.elasyes.projects@gmai.com>
2023-11-01 22:25:59 +0200

    would it help?!

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,27 +8,28 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *a;
-	int str_len, count = 0, len = 0;
+	int str_len1, str_len2, count, count2, len = 0;
 
 	if (s1 == NULL)
 		s1 = "";
-
 	if (s2 == NULL)
 		s2 = "";
-
-	for (str_len = 0; s1[str_len] || s2[str_len]; str_len++)
-		len++;
-
-	a = malloc(sizeof(char) * len);
-
+	for (str_len1 = 0; s1[str_len1]; str_len1++)
+	;
+	for (str_len2 = 1; s2[str_len2]; str_len2++)
+	;
+	a = malloc((str_len1 + str_len2 + 1) * sizeof(char));
 	if (a == NULL)
 		return (NULL);
-
-	for (str_len = 0; s1[str_len]; str_len++)
-		a[count++] = s1[str_len];
-
-	for (str_len = 0; s2[str_len]; str_len++)
-		a[count++] = s2[str_len];
-
+	for (count = 0; s1[count]; count++)
+	{
+		if (s1[count] == 0)
+		break;
+		a[count] = s1[count];
+	}
+	for (count2 = 0; s2[count2]; count2++)
+	{
+		a[count++] = s2[count2];
+	}
 	return (a);
 }
