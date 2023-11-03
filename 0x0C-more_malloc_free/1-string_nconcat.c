a56df58f5732ca38337f341508a54c1805b033c4
Mohamed <mr.elasyes.projects@gmai.com>
2023-11-03 23:33:09 +0200

    betty

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,32 +13,22 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (s1 == NULL)
 		s1 = "";
-
 	if (s2 == NULL)
 		s2 = "";
-
-	for (;s1[str_len1] != '\0'; str_len1++)
+	for (; s1[str_len1] != '\0'; str_len1++)
 	;
-
-	for (;s2[str_len2] != '\0'; str_len2++)
+	for (; s2[str_len2] != '\0'; str_len2++)
 	;
-
 	if (n > str_len2)
 	n = str_len2;
 	a = malloc((str_len1 + n + 1) * sizeof(char));
-
 	if (a == NULL)
 		return (0);
-
 	for (count = 0; count < str_len1; count++)
-	{
 		a[count] = s1[count];
-	}
 
 	for (; count < (str_len1 + n); count++)
-	{
 		a[count] = s2[count - str_len1];
-	}
 	a[count] = '\0';
 
 return (a);
