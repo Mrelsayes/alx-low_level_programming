3450f1c6518dd688818595f0c4a756c781dcb0ad
Mohamed <mr.elasyes.projects@gmai.com>
2023-11-07 17:48:52 +0200

    int not void

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 /**
  * main - prints the name of the file it was compiled from
-*/
-void main(void)
+ * Return: 0
+ */
+int main(void)
 {
 	printf("%s\n", __FILE__);
+	return (0);
 }
