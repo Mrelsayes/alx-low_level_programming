8736256f255527bb012f2cb0fe967955a5bcc824
Mohamed <mr.mohamedelsayes@gmai.com>
2023-11-27 20:39:51 +0200

    betty

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,7 +4,7 @@
  * create_file - creates a file
  * @filename: filename
  * @text_content: content in the file
- * Return: 1 || -1 
+ * Return: 1 || -1
  */
 int create_file(const char *filename, char *text_content)
 {
